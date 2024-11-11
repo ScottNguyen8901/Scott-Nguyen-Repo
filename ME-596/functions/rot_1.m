171cdf38e0abbd348b5f810d9ccf1706524d10df
Scott Nguyen <scott.nguyen8901@gmail.com>
2024-11-11 15:21:51 -0600

    Module 4 and exam 1 stuff

--- ME-596/functions/rot_1.m
+++ ME-596/functions/rot_1.m
@@ -1,17 +1,17 @@
 function R_1 = rot_1(theta)
-% DESCRIPTION
-%   Compute the rotation matrix for a rotation about the x-axis by an angle 
-%   theta. This function is used in coordinate transformations involving a 
-%   rotation around the first axis (x-axis).
-%
-% INPUTS        size    Type
-%   theta       (1,1)   Double  Angle of rotation [radians]
-%
-% OUTPUTS       size    Type
-%   R_1         (3,3)   Double  Rotation matrix for rotation about x-axis
-%
-% FUNCTION
-%
+    % DESCRIPTION
+    %   Compute the rotation matrix for a rotation about the x-axis by an angle 
+    %   theta. This function is used in coordinate transformations involving a 
+    %   rotation around the first axis (x-axis).
+    %
+    % INPUTS        size    Type
+    %   theta       (1,1)   Double  Angle of rotation [radians]
+    %
+    % OUTPUTS       size    Type
+    %   R_1         (3,3)   Double  Rotation matrix for rotation about x-axis
+    %
+    % FUNCTION
+    %
     c = cos(theta);
     s = sin(theta);
     
