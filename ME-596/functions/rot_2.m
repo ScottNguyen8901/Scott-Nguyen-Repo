e42d144c055290f19505729a9159afb7e9137f0c
Scott Nguyen <scott.nguyen8901@gmail.com>
2024-11-11 22:41:48 -0600

    HW 4

--- ME-596/functions/rot_2.m
+++ ME-596/functions/rot_2.m
@@ -1,18 +1,18 @@
 function R_2 = rot_2(theta)
-%
-% DESCRIPTION
-%   Compute the rotation matrix for a rotation about the y-axis by an angle 
-%   theta. This function is commonly used in coordinate transformations 
-%   where a rotation around the second axis (y-axis) is needed.
-%
-% INPUTS        size    Type
-%   theta       (1,1)   Double  Angle of rotation [radians]
-%
-% OUTPUTS       size    Type
-%   R_2         (3,3)   Double  Rotation matrix for rotation about y-axis
-%
-% FUNCTION
-%
+    %
+    % DESCRIPTION
+    %   Compute the rotation matrix for a rotation about the y-axis by an angle 
+    %   theta. This function is commonly used in coordinate transformations 
+    %   where a rotation around the second axis (y-axis) is needed.
+    %
+    % INPUTS        size    Type
+    %   theta       (1,1)   Double  Angle of rotation [radians]
+    %
+    % OUTPUTS       size    Type
+    %   R_2         (3,3)   Double  Rotation matrix for rotation about y-axis
+    %
+    % FUNCTION
+    %
 
     c = cos(theta);
     s = sin(theta);
--- ME-596/functions/rot_3.m
+++ ME-596/functions/rot_3.m
@@ -1,17 +1,17 @@
 function R_3 = rot_3(theta)
-% DESCRIPTION
-%   Compute the rotation matrix for a rotation about the z-axis by an angle 
-%   theta. This function is used in coordinate transformations involving a 
-%   rotation around the third axis (z-axis).
-%
-% INPUTS        size    Type
-%   theta       (1,1)   Double  Angle of rotation [radians]
-%
-% OUTPUTS       size    Type
-%   R_3         (3,3)   Double  Rotation matrix for rotation about z-axis
-%
-% FUNCTION
-%
+    % DESCRIPTION
+    %   Compute the rotation matrix for a rotation about the z-axis by an angle 
+    %   theta. This function is used in coordinate transformations involving a 
+    %   rotation around the third axis (z-axis).
+    %
+    % INPUTS        size    Type
+    %   theta       (1,1)   Double  Angle of rotation [radians]
+    %
+    % OUTPUTS       size    Type
+    %   R_3         (3,3)   Double  Rotation matrix for rotation about z-axis
+    %
+    % FUNCTION
+    %
     c = cos(theta);
     s = sin(theta);
     
