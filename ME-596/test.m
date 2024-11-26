6fb6952378ce457edadfc21c68189b02577ac1df
Scott Nguyen <scott.nguyen8901@gmail.com>
2024-11-25 21:50:02 -0600

    HW 6 stuff

--- ME-596/test.m
+++ ME-596/test.m
@@ -1,126 +1,64 @@
+close
 clear
 clc
 
-% Dimensions and masses
-a = 1; % Prism base width
-b = 1; % Prism base height
-h = 2; % Prism height
-M = 100; % Prism mass
+% Given quantities
+% Cylinder
+d = 1;            % [m]
+r = d / 2;        % [m]
+h = 2;            % [m]
+mu = 100;         % [kg / m^3]
+V = pi * r^2 * h; % [m^3]
+m_cyl = mu * V;   % [kg]
 
-L = 2; % Panel length
-w = 0.6; % Panel width
-m = 10; % Panel mass
+% Rod
+l = 2;     % [m]
+m_rod = 1; % [kg]
 
-% Offset for panels
-r = (a + L) / 2;
+% Panel
+L = 2;               % [m]
+w = 0.5;             % [m]
+m_pan = 4;           % [kg]
+theta = deg2rad(87); % [rad]
 
-% Rectangular prism moment of inertia
-I_rp = (M / 12) * diag([b^2 + h^2, a^2 + h^2, a^2 + b^2]);
+% Mass centers of components about center of cylinder
+r_cyl_c_b = [0; 0; 0];
+r_rod_c_b = [0; r + l / 2; h];
+r_pan_c_b = [0; r + l / 2; h];
 
-% Panel moment of inertia
-I_p = (m / 12) * diag([w^2 + L^2, w^2, w^2]);
+% Moment of Inertia Calculations about mass centers
+I_cyl_c_b = m_cyl * diag([(3 * r^2 + h^2) / 12; ...
+                      (3 * r^2 + h^2) / 12; ...
+                      r^2 / 2]);
 
-% Offset vector for first panel
-r_vec1 = [0; -r; 0];
-I_panel1 = I_p + m * (r^2 * eye(3) - r_vec1 * r_vec1');
+I_rod_c_b = m_rod * diag([l^2 / 12, 0, l^2 / 12]);
 
-% Offset vector for second panel
-r_vec2 = [0; r; 0];
-I_panel2 = I_p + m * (r^2 * eye(3) - r_vec2 * r_vec2');
+I_pan_c_a = m_pan * diag([(L + w)^2 / 12; ...
+                          w^2 / 12; ...
+                          L^2 / 12]);
 
-% Total moment of inertia tensor
-I_o = I_rp + I_panel1 + I_panel2;
+% Change of Base Vector
+R_ba = rot_2(theta);
+I_pan_c_b = R_ba * I_pan_c_a * R_ba';
 
-%%
-clear
-clc
+% Parallel Axis Theorem
+I_cyl_o_b = I_cyl_c_b - m_cyl * skew(r_cyl_c_b) * skew(r_cyl_c_b);
+I_rod_o_b = I_rod_c_b - m_rod * skew(r_rod_c_b) * skew(r_rod_c_b);
+I_pan_o_b = I_pan_c_b - m_pan * skew(r_pan_c_b) * skew(r_pan_c_b);
 
-I = [51.6294, 1.8192, 0.4055;
-     1.8192, 41.2647, 0.6444;
-     0.4055, 0.6444, 31.9150];
+% Total Moment of Inertia about Center of Cylinder
+I_comp_o_b = I_cyl_o_b + I_rod_o_b + I_pan_o_b;
 
-[V,D]=eig(I)
+% Location of center of mass about point o 
+M = m_cyl + m_rod + m_pan;
+r_c_o = (m_cyl * r_cyl_c_b + m_rod * r_rod_c_b + m_pan * r_pan_c_b) / M;
 
-%%
-% Given moment of inertia matrix
-I = [181.2123, 9.1086, 10.6243;
-     9.1086, 192.0960, 11.1512;
-     10.6243, 11.1512, 44.8092];
+% Total Moment of Inertia about Center of Mass
+I_comp_c_b = I_comp_o_b - M * skew(r_c_o) * skew(r_c_o);
 
-% Calculate the eigenvalues of the matrix
-eigenvalues = eig(I);
+[axes_new, mois_new] = reorder_axes_and_mois(I_comp_c_b);
 
-% The minor axis moment of inertia is the smallest eigenvalue
-minor_axis_moi = min(eigenvalues);
+% Converting rotation matrix to euler angle and axis
+[phi, a] = rot_to_eul(axes_new);
 
-% Display the result
-disp(['Minor axis moment of inertia: ', num2str(minor_axis_moi)]);
-
-%%
-% Given moment of inertia matrix
-I = [181.2123, 9.1086, 10.6243;
-     9.1086, 192.0960, 11.1512;
-     10.6243, 11.1512, 44.8092];
-
-% Calculate the eigenvalues of the matrix
-eigenvalues = eig(I);
-
-% The major axis moment of inertia is the largest eigenvalue
-major_axis_moi = max(eigenvalues);
-
-% Display the result
-disp(['Major axis moment of inertia: ', num2str(major_axis_moi)]);
-
-%%
-% Given moment of inertia matrix
-I = [181.2123, 9.1086, 10.6243;
-     9.1086, 192.0960, 11.1512;
-     10.6243, 11.1512, 44.8092];
-
-% Calculate the eigenvalues and eigenvectors of the matrix
-[eigenvectors, eigenvalues] = eig(I);
-
-% Find the index of the smallest eigenvalue (minor axis)
-[~, minor_index] = min(diag(eigenvalues));
-
-% The corresponding eigenvector is the unstable spin axis
-unstable_spin_axis = eigenvectors(:, minor_index);
-
-% Display the result
-disp('Unstable spin axis:');
-disp(unstable_spin_axis);
-
-%%
-
-clear
-clc
-
-% Dimensions and masses
-a = 1; % Prism base width
-b = 1; % Prism base height
-h = 2; % Prism height
-M = 100; % Prism mass
-
-L = 2; % Panel length
-w = 0.6; % Panel width
-m = 10; % Panel mass
-
-% Offset for panels
-r = (a + L) / 2;
-
-% Rectangular prism moment of inertia
-I_rp = (M / 12) * diag([b^2 + h^2, a^2 + h^2, a^2 + b^2]);
-
-% Panel moment of inertia
-I_p = (m / 12) * diag([w^2 + L^2, w^2, w^2]);
-
-% Offset vector for first panel
-r_vec1 = [0; -r; 0];
-I_panel1 = I_p + m * (r^2 * eye(3) - r_vec1 * r_vec1');
-
-% Offset vector for second panel
-r_vec2 = [0; r; 0];
-I_panel2 = I_p + m * (r^2 * eye(3) - r_vec2 * r_vec2');
-
-% Total moment of inertia tensor
-I_o = I_rp + I_panel1 + I_panel2;
+phi = rad2deg(phi);
