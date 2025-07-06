b9a375bf6a4a84dd8fcb7444eaedb595ee21efb2
Scott Nguyen <scott.nguyen8901@gmail.com>
2025-07-06 13:17:02 -0600

    Hardened code

--- UNM-SSE/[3]-Summer-2025/ECE-531/hw/hw_2/time_daemon.c
+++ UNM-SSE/[3]-Summer-2025/ECE-531/hw/hw_2/time_daemon.c
@@ -1,4 +1,5 @@
-// cc -o time_daemon time_daemon.c
+// cc -D_FORTIFY_SOURCE=2 -O2 -Wall -Wextra -o time_daemon time_daemon.c
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,13 +8,38 @@
 #include <syslog.h>
 #include <time.h>
 #include <signal.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/resource.h>
 
-static volatile int running = 1;
+static volatile sig_atomic_t running = 1;
 
 void signal_handler(int sig) {
     if (sig == SIGTERM || sig == SIGINT) {
         running = 0;
     }
+}
+
+// Minimal privilege drop function
+void drop_privileges() {
+    if (setuid(65534) != 0) {  // 65534 = nobody user on most systems
+        syslog(LOG_ERR, "Failed to drop privileges: %s", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+}
+
+// Set resource limits (defensive)
+void set_limits() {
+    struct rlimit rl;
+
+    rl.rlim_cur = 10 * 1024 * 1024;  // 10 MB max memory usage
+    rl.rlim_max = 10 * 1024 * 1024;
+    setrlimit(RLIMIT_AS, &rl);
+
+    rl.rlim_cur = 1024;  // max open files
+    rl.rlim_max = 1024;
+    setrlimit(RLIMIT_NOFILE, &rl);
 }
 
 int main() {
@@ -22,51 +48,65 @@ int main() {
     // Fork the parent process
     pid = fork();
     if (pid < 0) {
+        perror("fork failed");
         exit(EXIT_FAILURE);
     }
     if (pid > 0) {
-        // Exit the parent process
         exit(EXIT_SUCCESS);
     }
 
-    // Create a new session ID for the child process
+    // Create a new session ID
     sid = setsid();
     if (sid < 0) {
+        perror("setsid failed");
         exit(EXIT_FAILURE);
     }
 
-    // Change working directory to root
-    if ((chdir("/")) < 0) {
+    // Change working directory to /
+    if (chdir("/") < 0) {
+        perror("chdir failed");
         exit(EXIT_FAILURE);
     }
 
-    // Close standard file descriptors
+    // Close file descriptors
     close(STDIN_FILENO);
     close(STDOUT_FILENO);
     close(STDERR_FILENO);
 
-    // Setup signal handling to allow clean shutdown
-    signal(SIGTERM, signal_handler);
-    signal(SIGINT, signal_handler);
+    // Set restrictive umask
+    umask(027);
 
-    // Open connection to syslog
-    openlog("time_daemon", LOG_PID, LOG_DAEMON);
+    // Signal handling
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = signal_handler;
+    sigaction(SIGTERM, &sa, NULL);
+    sigaction(SIGINT, &sa, NULL);
+
+    // Set resource limits
+    set_limits();
+
+    // Open syslog
+    openlog("time_daemon", LOG_PID | LOG_CONS, LOG_DAEMON);
+    syslog(LOG_INFO, "Daemon started securely");
+
+    // Drop privileges
+    drop_privileges();
 
     while (running) {
         time_t now = time(NULL);
         if (now == ((time_t) -1)) {
             syslog(LOG_ERR, "Failed to get current time");
         } else {
-            struct tm *ptm = localtime(&now);
-            if (ptm == NULL) {
-                syslog(LOG_ERR, "Failed to convert time to local time");
+            struct tm ptm;
+            char time_str[64];
+
+            if (localtime_r(&now, &ptm) == NULL) {
+                syslog(LOG_ERR, "Failed to convert time");
+            } else if (strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", &ptm) == 0) {
+                syslog(LOG_ERR, "strftime returned 0");
             } else {
-                char time_str[64];
-                if (strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", ptm) == 0) {
-                    syslog(LOG_ERR, "strftime returned 0");
-                } else {
-                    syslog(LOG_INFO, "Current time: %s", time_str);
-                }
+                syslog(LOG_INFO, "Current time: %s", time_str);
             }
         }
         sleep(1);
